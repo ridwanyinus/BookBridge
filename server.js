263ef6eed3f7cfc86d6cf5f89ca7ab6aa778e561
Ridwan <ridwanyinusa15@gmail.com>
2024-11-26 17:46:56 +0100

    chore: add localhost route

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 
 let searchBook = [];
 
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
 app.get('/api/admin', (req, res) => {
   const id = req.query.bookId;
   res.json(searchBook[id]);
