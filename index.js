083c96085691d53d535fca7fff8d61022445e00d
Ridwan <ridwanyinusa15@gmail.com>
2024-11-26 19:18:50 +0100

    test: test cors(dev)

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://bookbridge-9res.onrender.com'],
+  }),
+);
 
 let searchBook = [];
 
